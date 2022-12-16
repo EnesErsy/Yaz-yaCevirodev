83ff40e7f50b5a141be6c2b1c1008f4368b15701
EnesErsy <120748743+EnesErsy@users.noreply.github.com>
2022-12-16 22:48:41 +0300

    YazıyaCevirOdev
    221809014-EnesErsoy

---
+++ YazıyaCevirOdev/YazıyaCevirOdev/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace YazıyaCevirOdev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a ;
+            string s = "";
+            string[] BirlerBasamagı = { "Bir", "İki", "Üç", "Dört", "Beş", "Altı", "Yedi", "Sekiz", "Dokuz" };
+            string[] OnlarBasamagı = { "On", "Yirmi", "Otuz", "Kırk", "Elli", "Altmış", "Yetmiş", "Seksen", "Doksan" };
+            string[] YuzlerBasamagı = { "Yüz", "İkiyüz", "Üçyüz", "Dörtyüz", "Beşyüz", "Altıyüz", "YediYüz", "Sekizyüz", "Dokuzyüz" };
+            Console.Write("Bir Sayı Giriniz");
+            a=int.Parse(Console.ReadLine());
+            if (a / 100 >= 1) 
+            {
+                s += YuzlerBasamagı[(a / 100) - 1];
+                a = a % 100;
+            }
+            if (a / 10 >= 1)
+            {
+                s += OnlarBasamagı[(a / 10) - 1];
+
+                a = a % 10;
+            }
+            if (a % 10 != 0)
+            {
+                s += BirlerBasamagı[(a % 10) - 1];
+            }
+            Console.WriteLine(s);
+            Console.WriteLine("Enter Tuşuna Basınız");
+            Console.ReadKey(true);
+        }
+    }
+}
